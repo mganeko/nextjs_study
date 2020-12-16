37ef35a2b6b6aedd4cc0aa9551ff3d16da9ab706
mganeko <m.ganeko@gmail.com>
2020-12-16 19:08:05 +0900

    use effect with func

---
+++ src/pages/clockfunc.tsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+
+function Clock() {
+  const [date, setDate] = useState(new Date());
+
+  //Replaces componentDidMount and componentWillUnmount
+  useEffect(() => {
+    const timerID = setInterval( () => tick(), 1000 );
+
+    return function cleanup() {
+      clearInterval(timerID);
+    };
+  });
+
+  function tick() {
+    setDate(new Date());
+  }
+
+  return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {date.toLocaleTimeString()}.</h2>
+      </div>
+  );
+}
+
+export default Clock;
