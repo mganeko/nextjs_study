fa823de0d9ad678d1c4ac4a4a8fa07d92d81fdc0
mganeko <m.ganeko@gmail.com>
2020-12-16 18:39:57 +0900

    use state of react

---
+++ src/pages/nowstate.tsx
@@ -0,0 +1,19 @@
+//
+// page
+//  run on client side
+//  (also run on server side in dev mode, but override by client side)
+//
+
+import { useState } from 'react';
+
+export default function Index() {
+  const [nowStr, setState] = useState(Date() + ' state');
+
+  //const host = process.env.HOSTNAME;
+  //const env = process.env.NODE_ENV;
+  const env = process.env.TEST_VAR;
+  const nowDate = Date();
+  //const nowStr = 'nowStr:' + nowDate;
+  console.log('Page NODE_ENV=%s now=%s', env, nowDate, nowStr);
+  return <h1>nodeEnv:{env}, {nowStr}</h1>;
+}
